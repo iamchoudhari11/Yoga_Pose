9d9bf9884587d6b2f52469850ec7a418ad6f8bd0
unknown <geet.choudhari1999@gmail.com>
2021-06-02 00:02:53 +0530

    Heroku deploy

--- my_app.py
+++ my_app.py
@@ -5,13 +5,13 @@ from blazepose import blazepose
 from openpose import openpose
 from PIL import Image
 
-icon = Image.open("images/icon.jpg")
-st.set_page_config(
-    page_title="REAL TIME HUMAN POSE ESTIMATION:",
-    page_icon=icon,
-    layout="wide",
-    initial_sidebar_state="auto",
-)
+# icon = Image.open("images/icon.jpg")
+# st.set_page_config(
+#     page_title="REAL TIME HUMAN POSE ESTIMATION:",
+#     page_icon=icon,
+#     layout="wide",
+#     initial_sidebar_state="auto",
+# )
 
 st.sidebar.markdown("<h1 style='color:Red ;'>Navigation</h1>", unsafe_allow_html=True)
 page = st.sidebar.radio(" ",("Home","BlazePose","OpenPose"))
